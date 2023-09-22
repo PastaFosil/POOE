b7637e777bec82fb5aa1d21e25a33f5b21da07e9
juancho <juancrlos6@gmail..com>
2023-09-21 19:29:27 -0600

    Adding homework

---
+++ Tareas/JuanCarlosPerezRamirez_tarea3.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+void sortAsc(int *arr, int size) {
+    int pass = 0;
+    while (pass == 0) {
+        pass = 1;
+        for (int i=0; i<size-1; i++) {
+            int temp = arr[i+1];
+            if (arr[i]>arr[i+1]) {
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+                pass = 0;
+            }
+        }
+        
+    }
+}
+
+int main()
+{
+    cout<<"Define int array: \n\n";
+    
+    cout << "Size: ";
+    int n;
+    cin >> n;
+    
+    int* array = new int[n];
+    
+    cout << endl;
+    
+    for (int i=0; i<n; i++) {
+        cout << "array[" << i <<"] = ";
+        cin >> array[i];
+    }
+    
+    sortAsc(array, n);
+    
+    cout << "\nSorted array: " << "{ ";
+    for (int i=0; i<n; i++) {
+        if (i<n-1) {
+            cout << array[i] << " , ";
+        } else {
+            cout << array[i] << " }";
+        }
+        
+    }
+    return 0;
+}
+
+
+
